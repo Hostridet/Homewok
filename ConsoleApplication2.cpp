6af6f5a19a1269fdc9194e8a0e60cc87e1431e62
Румянцев Никита <rumyancev-2001@mail.ru>
2020-10-02 21:15:55 +1000

    my 1st commit

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,32 @@
+﻿
+#include <iostream>
+#include <stdio.h>
+
+int main()
+{
+    char a;
+    _asm
+    {
+        mov al, 1100b
+        mov bh, 1010b
+        add al, bh
+        mov bh, 0
+        mov cl, 1000000b
+        mov bl, 0000001b
+        shr cl, 6
+        shl bl, 6
+        xor al, cl
+        xor al, bl
+        mov cl, 010000b
+        mov bl, 000010b
+        shr cl, 3
+        shl bl, 3
+        xor al, bl
+        xor al, cl
+        mov a, al
+    }
+    printf("%d", a);
+        getchar();
+}
+
+
